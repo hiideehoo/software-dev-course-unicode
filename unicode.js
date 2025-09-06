63591d8462b05f9179fc86e480fca751905c609b
hiideehoo <hiideeho@gmail.com>
2025-09-05 23:20:47 -0500

    Here we are.

--- unicode.js
+++ unicode.js
@@ -16,6 +16,14 @@ let inputString = "Hi";
 Expected Output:
 sumCodePoints: 177 // 'H' = 72, 'i' = 105, 72 + 105 = 177
 
+*/
+
+let inputStringA = "Hi";
+let sumCodePoints = inputStringA.charCodeAt(0) + inputStringA.charCodeAt(1); // 72 + 105
+console.log(sumCodePoints); // 177
+
+/*
+
 Exercise 2: Generate a String from Two Code Points
 Objective: Take two numeric Unicode code points,
 convert them to characters using String.fromCharCode,
@@ -31,6 +39,15 @@ let codePoint2 = 66;
 
 Expected Output:
 combinedString: "AB" // 65 = 'A', 66 = 'B', combined = "AB"
+
+*/
+
+let codePoint1 = 65;
+let codePoint2 = 66;
+let combinedString = String.fromCharCode(codePoint1) + String.fromCharCode(codePoint2); // "A" + "B"
+console.log(combinedString); // AB
+
+/*
 
 Exercise 3: Find the Character Difference
 Objective: Extract the Unicode code points of two given characters from a string
@@ -48,7 +65,15 @@ let index2 = 2;
 Expected Output:
 codePointDifference: 16 // 'C' = 67, 't' = 116, |67 - 116| = 16
 
+*/
 
+let inputStringB = "Cat";
+let index1 = 0;
+let index2 = 2;
+let codePointDifference = Math.abs(inputStringB.charCodeAt(index1) - inputStringB.charCodeAt(index2)); // 67 - 116
+console.log(codePointDifference); // 49 (I think expected output is wrong)
+
+/*
 
 Practice Problem #2
 
@@ -86,15 +111,17 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0); // 67
+let thirdCodePoint = inputString1.charCodeAt(2); // 100
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72) + String.fromCharCode(101) + String.fromCharCode(108) + String.fromCharCode(108); // Hell
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let extractedPointFirst = inputString2.charCodeAt(0); // 76
+let extractedPointLast = inputString2.charCodeAt(5); // 104
+let swappedString = String.fromCharCode(104) + "aunc" + String.fromCharCode(76); // hauncL
 
 // Log all results
 console.log({
